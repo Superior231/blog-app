c4dfab189dc47d6f6a8cf09c5ee70260dcf81d19
Hikmal Falah <ikmalfalah@gmail.com>
2024-07-14 08:02:31 +0700

    Update {style}: summernote background color

--- resources/views/components/style.blade.php
+++ resources/views/components/style.blade.php
@@ -1,7 +1,7 @@
 <link rel="shortcut icon" href="{{ url('assets/images/logo.png') }}" type="image/x-icon">
 <link rel="stylesheet" href="{{ url('assets/vendors/bootstrap/css/bootstrap.css') }}">
 <link rel="stylesheet" href="{{ url('assets/vendors/boxicons/css/boxicons.min.css') }}">
-<link rel="stylesheet" href="{{ url('assets/css/styles.css') }}">
+<link rel="stylesheet" href="{{ url('assets/css/style.css') }}">
 @stack('styles')
 
 <title>{{ $title }}</title>
