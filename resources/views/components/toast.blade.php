8eceb2bd301022af386604b723f283f532417903
Hikmal Falah <ikmalfalah@gmail.com>
2024-07-28 02:08:11 +0700

    Update {style}: toast

--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -1,31 +1,30 @@
 {{-- Success --}}
-<div class="position-fixed p-3" id="toast-success">
-    <div class="toast align-items-center" id="toast-success-content" role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="d-flex">
-            <div class="toast-body d-flex align-items-center gap-2">
-                <i class='bx bxs-check-circle text-success fs-2'></i>
-                <span class="ms-2 my-0 py-0 fw-medium">{{ session()->get('success') }}</span>
+@if (session('success'))
+    <div class="position-fixed p-3" id="toast-success">
+        <div class="toast align-items-center" id="toast-success-content" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="d-flex">
+                <div class="toast-body d-flex align-items-center gap-2">
+                    <i class='bx bxs-check-circle text-success fs-2'></i>
+                    <span class="ms-2 my-0 py-0 fw-medium">{{ session('success') }}</span>
+                </div>
+                <button type="button" class="btn-close me-3 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
-            <button type="button" class="btn-close me-3 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
-    </div>
-</div>
-
-@if (session('status'))
-    <div class="alert alert-success">
-        {{ session('status') }}
     </div>
 @endif
 
+
 {{-- Error --}}
-<div class="position-fixed p-3" id="toast-error">
-    <div class="toast toast-error align-items-center" id="toast-error-content" role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="d-flex">
-            <div class="toast-body d-flex align-items-center gap-2">
-                <i class='bx bxs-x-circle text-danger fs-2'></i>
-                <span class="ms-2 my-0 py-0 fw-medium">{{ session()->get('error') }}</span>
+@if (session('error'))
+    <div class="position-fixed p-3" id="toast-error">
+        <div class="toast toast-error align-items-center" id="toast-error-content" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="d-flex">
+                <div class="toast-body d-flex align-items-center gap-2">
+                    <i class='bx bxs-x-circle text-danger fs-2'></i>
+                    <span class="ms-2 my-0 py-0 fw-medium">{{ session('error') }}</span>
+                </div>
+                <button type="button" class="btn-close me-3 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
-            <button type="button" class="btn-close me-3 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
     </div>
-</div>
+@endif
