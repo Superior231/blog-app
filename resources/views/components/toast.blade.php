ff8e186b84c0dd6d6b15287fa255ceec2605e5ae
Hikmal Falah <ikmalfalah@gmail.com>
2024-07-28 02:36:57 +0700

    Update {style}: toast

--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -28,3 +28,15 @@
         </div>
     </div>
 @endif
+
+@push('scripts')
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+            var toastList = toastElList.map(function (toastEl) {
+                return new bootstrap.Toast(toastEl)
+            })
+            toastList.forEach(toast => toast.show());
+        });
+    </script>
+@endpush
